fe14cd62e93ef527017830e303f4852b4c21df76
Your Name <you@example.com>
2024-09-17 17:25:32 +0530

    Rendered Model and animations

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useThree } from "@react-three/fiber";
 
 export const Experience = () => {
+  const texture = useTexture("/textures/Background.jpeg");
+  const viewport = useThree((state) => state.viewport);
+
   return (
     <>
       <OrbitControls />
+      <Avatar position={[0, -3, 5]} scale={2} />
+      <Environment preset="sunset" />
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        <planeGeometry args={[viewport.width, viewport.height]} />
+        <meshStandardMaterial map={texture} />
       </mesh>
     </>
   );
